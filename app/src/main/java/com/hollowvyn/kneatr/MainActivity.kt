bf2a427f11f2eda964c9eb0c12bd36ecd038dc83
Tobi Olutimehin <olutimehintobi@gmail.com>
2025-07-07 21:38:36 -0400

    Git init

---
+++ app/src/main/java/com/hollowvyn/kneatr/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.hollowvyn.kneatr
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hollowvyn.kneatr.ui.theme.KneatrTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KneatrTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Kneatr, the app to keep connections",
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GreetingPreview() {
+    KneatrTheme {
+        Greeting("Kneatr, the app to keep connections")
+    }
+}
