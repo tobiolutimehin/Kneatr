c35174094bd3de02dcf2a7663062ed9bb7af5883
Tobi Olutimehin <olutimehintobi@gmail.com>
2025-07-08 00:19:31 -0400

    Update target sdk
    Signed-off-by: Tobi Olutimehin <olutimehintobi@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.hollowvyn.kneatr"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.hollowvyn.kneatr"
         minSdk = 30
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
