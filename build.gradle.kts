3d9042d31852fcd49d12d6702f4f7264937de1ee
Tobi Olutimehin <olutimehintobi@gmail.com>
2025-07-13 18:51:34 -0400

    Refactor: Reorganize dependencies in Gradle files
    This commit reorganizes the `gradle/libs.versions.toml`, `build.gradle.kts`, and `app/build.gradle.kts` files.
    
    - In `gradle/libs.versions.toml`:
        - Grouped versions and libraries by category (Core, AndroidX Core, AndroidX Lifecycle, AndroidX Activity / Compose, AndroidX Navigation, Testing, DI / Tooling, Serialization).
    - In `build.gradle.kts` and `app/build.gradle.kts`:
        - Added comments to clarify plugin groups.
        - Reordered plugins and dependencies to align with the new categories in `libs.versions.toml`.
    
    Signed-off-by: Tobi Olutimehin <olutimehintobi@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
+    // Android Application Plugin
     alias(libs.plugins.android.application) apply false
+
+    // Kotlin Plugins
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.jetbrains.kotlin.serialization) apply false
+
+    // Dependency Injection (Hilt)
     alias(libs.plugins.hilt.android) apply false
+
+    // Kotlin Symbol Processing (KSP)
     alias(libs.plugins.ksp) apply false
 }
