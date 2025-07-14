710cbbbecf1fba5a41c9d7f2b26952e55db82bf7
Tobi Olutimehin <olutimehintobi@gmail.com>
2025-07-13 22:50:08 -0400

    feat: create kneatr database
    This commit introduces the `KneatrDatabase` class, an abstract class that extends `RoomDatabase`. It defines the entities for the database, including `ContactEntity`, `ContactTierEntity`, `TagEntity`, and `ContactTagCrossRef`. The database version is set to 1, and schema export is disabled.
    
    The `KneatrDatabase` class also includes abstract functions for accessing the DAOs: `contactDao()`, `contactTierDao()`, `tagDao()`, and `contactTagCrossRefDao()`. Additionally, it utilizes `LocalDateConverters` for type conversion.
    
    Signed-off-by: Tobi Olutimehin <olutimehintobi@gmail.com>

---
+++ app/src/main/java/com/hollowvyn/kneatr/data/local/database/KneatrDatabase.kt
@@ -0,0 +1,35 @@
+package com.hollowvyn.kneatr.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.hollowvyn.kneatr.data.local.dao.ContactDao
+import com.hollowvyn.kneatr.data.local.dao.ContactTagCrossRefDao
+import com.hollowvyn.kneatr.data.local.dao.ContactTierDao
+import com.hollowvyn.kneatr.data.local.dao.TagDao
+import com.hollowvyn.kneatr.data.local.entity.ContactEntity
+import com.hollowvyn.kneatr.data.local.entity.ContactTierEntity
+import com.hollowvyn.kneatr.data.local.entity.TagEntity
+import com.hollowvyn.kneatr.data.local.entity.crossRef.ContactTagCrossRef
+import com.hollowvyn.kneatr.data.local.typeconverter.LocalDateConverters
+
+@Database(
+    entities = [
+        ContactEntity::class,
+        ContactTierEntity::class,
+        TagEntity::class,
+        ContactTagCrossRef::class,
+    ],
+    version = 1,
+    exportSchema = false,
+)
+@TypeConverters(LocalDateConverters::class)
+abstract class KneatrDatabase : RoomDatabase() {
+    abstract fun contactDao(): ContactDao
+
+    abstract fun contactTierDao(): ContactTierDao
+
+    abstract fun tagDao(): TagDao
+
+    abstract fun contactTagCrossRefDao(): ContactTagCrossRefDao
+}
