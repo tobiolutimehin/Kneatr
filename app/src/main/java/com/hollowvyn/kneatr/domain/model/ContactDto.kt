cb70be4a79ead9a19659abbf8f109701a1ec2d98
Tobi Olutimehin <olutimehintobi@gmail.com>
2025-07-19 14:47:53 -0400

    Update TODOs for ContactDto
    The ContactDto class was updated to clarify that support for multiple phone numbers and emails is planned for a later version.
    
    Signed-off-by: Tobi Olutimehin <olutimehintobi@gmail.com>

--- app/src/main/java/com/hollowvyn/kneatr/domain/model/ContactDto.kt
+++ app/src/main/java/com/hollowvyn/kneatr/domain/model/ContactDto.kt
@@ -6,8 +6,8 @@ import kotlinx.datetime.LocalDate
 data class ContactDto(
     val id: Long,
     val name: String,
-    val phoneNumber: String,
-    val email: String?,
+    val phoneNumber: String, // TODO: support multiple phone numbers later
+    val email: String?, // TODO: support multiple emails later
     val tags: Set<ContactTagDto> = emptySet(),
     val communicationLogs: List<CommunicationLogDto> = emptyList(),
     val tier: ContactTierDto? = null,
