9d2b6ef970073511d226939ca9869f0da19e2a29
Tobi Olutimehin <olutimehintobi@gmail.com>
2025-09-08 02:27:01 -0400

    feat: Provide WorkManager via Hilt
    This commit introduces a new provider in `AppModule.kt` to make `WorkManager` available for dependency injection using Hilt.
    
    The `provideWorkManager` function takes the application `Context` as a parameter and returns an instance of `WorkManager` obtained via `WorkManager.getInstance(context)`.
    
    Signed-off-by: Tobi Olutimehin <olutimehintobi@gmail.com>

--- app/src/main/java/com/hollowvyn/kneatr/MainActivity.kt
+++ app/src/main/java/com/hollowvyn/kneatr/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.hollowvyn.kneatr
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -17,12 +17,19 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
+import androidx.core.content.edit
 import com.hollowvyn.kneatr.ui.contact.ContactsListScreen
 import com.hollowvyn.kneatr.ui.theme.KneatrTheme
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var prefsManager: PrefsManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,6 +40,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ContactsListScreen(modifier = Modifier.padding(innerPadding))
                 }
+//                GoToSettingsPermissionScreen()
             }
         }
     }
@@ -47,10 +55,7 @@ class MainActivity : ComponentActivity() {
                 ActivityResultContracts.RequestPermission(),
             ) { isGranted: Boolean ->
                 if (isGranted) {
-                    Log.d("ExampleScreen", "PERMISSION GRANTED")
-                    application.schedulePeriodicContactSync(context)
-                } else {
-                    Log.d("ExampleScreen", "PERMISSION DENIED")
+                    application.triggerImmediateContactSync(context)
                 }
             }
 
@@ -61,11 +66,30 @@ class MainActivity : ComponentActivity() {
                     Manifest.permission.READ_CONTACTS,
                 )
             ) {
-                Log.d("ExampleScreen", "PERMISSION ALREADY GRANTED")
+                if (prefsManager.isFirstLaunch) {
+                    application.triggerImmediateContactSync(context)
+                    prefsManager.isFirstLaunch = false // Mark it as done.
+                }
             } else {
-                Log.d("ExampleScreen", "REQUESTING PERMISSION")
                 launcher.launch(Manifest.permission.READ_CONTACTS)
             }
         }
     }
+}
+
+@Singleton
+class PrefsManager
+    @Inject
+    constructor(
+        @ApplicationContext context: Context,
+    ) {
+        private val prefs = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+
+        companion object {
+            private const val KEY_IS_FIRST_RUN = "is_first_run"
+        }
+
+        var isFirstLaunch: Boolean
+            get() = prefs.getBoolean(KEY_IS_FIRST_RUN, true)
+            set(value) = prefs.edit { putBoolean(KEY_IS_FIRST_RUN, value) }
 }
--- app/src/main/java/com/hollowvyn/kneatr/ui/contact/ContactsListViewModel.kt
+++ app/src/main/java/com/hollowvyn/kneatr/ui/contact/ContactsListViewModel.kt
@@ -1,59 +1,49 @@
 package com.hollowvyn.kneatr.ui.contact
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import com.hollowvyn.kneatr.data.CONTACT_SYNC_TAG
 import com.hollowvyn.kneatr.data.repository.ContactsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ContactsListViewModel
     @Inject
     constructor(
-        private val contactsRepository: ContactsRepository,
+        contactsRepository: ContactsRepository,
+        workManager: WorkManager,
     ) : ViewModel() {
-        private val _uiState = MutableStateFlow<ContactsListUiState>(ContactsListUiState.Empty)
-        val uiState: StateFlow<ContactsListUiState> = _uiState.asStateFlow()
+        private val workInfoFlow = workManager.getWorkInfosByTagFlow(CONTACT_SYNC_TAG)
+        private val contactsFlow = contactsRepository.getAllContacts()
 
-        fun getAllContacts() {
-            viewModelScope.launch {
-                _uiState.value = ContactsListUiState.Loading
-                try {
-                    val value = contactsRepository.getAllContacts().first()
-                    Log.d("ContactsListViewModel", "getAllContacts in viewmodel: $value")
-                    _uiState.value = ContactsListUiState.Success(value)
-                } catch (_: Exception) {
-                    _uiState.value = ContactsListUiState.Error
-                }
-            }
-        }
-
-        fun searchContacts(query: String) {
-            viewModelScope.launch {
-                try {
-                    val value = contactsRepository.searchContactsByNamePhoneOrEmail(query)
-
-                    if (_uiState.value is ContactsListUiState.Success) {
-                        _uiState.update {
-                            (it as ContactsListUiState.Success).copy(
-                                searchedContacts = value,
-                            )
-                        }
+        val uiState: StateFlow<ContactsListUiState> =
+            combine(contactsFlow, workInfoFlow) { contacts, workInfoList ->
+                val isWorkerRunning =
+                    workInfoList.any {
+                        it.state == WorkInfo.State.RUNNING || it.state == WorkInfo.State.ENQUEUED
                     }
-                } catch (_: Exception) {
-                    Log.e("ContactsListViewModel", "Error searching contacts")
-                }
-            }
-        }
 
-        init {
-            getAllContacts()
-        }
+                val didWorkerFail = workInfoList.any { it.state == WorkInfo.State.FAILED }
+
+                if (isWorkerRunning && contacts.isEmpty()) {
+                    ContactsListUiState.Loading
+                } else if (contacts.isNotEmpty()) {
+                    ContactsListUiState.Success(contacts)
+                } else if (didWorkerFail) {
+                    ContactsListUiState.Error
+                } else {
+                    ContactsListUiState.Empty
+                }
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ContactsListUiState.Loading,
+        )
     }
