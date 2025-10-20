378eda36ee56fd65752aa11a250cd0d1590bd69f
Tobi Olutimehin <olutimehintobi@gmail.com>
2025-10-19 20:19:28 -0400

    test: Rename LocalDateConverters test methods
    This commit updates the test methods in `LocalDateConvertersTest` to align with the renaming of a converter function.
    
    The function `fromEpochDays` was renamed to `toLocalDate` in the `LocalDateConverters` class, and this commit updates the corresponding test calls from `LocalDateConverters.fromEpochDays(...)` to `LocalDateConverters.toLocalDate(...)` to ensure the tests remain valid.
    
    Signed-off-by: Tobi Olutimehin <olutimehintobi@gmail.com>

--- app/src/test/java/com/hollowvyn/kneatr/data/local/dao/typeconverter/LocalDateConvertersTest.kt
+++ app/src/test/java/com/hollowvyn/kneatr/data/local/dao/typeconverter/LocalDateConvertersTest.kt
@@ -10,7 +10,7 @@ class LocalDateConvertersTest {
     fun `fromEpochDay converts correctly`() {
         val date = LocalDate(2025, 1, 1)
         val epochDay = LocalDateConverters.localDateToEpochDays(date)
-        val restored = LocalDateConverters.fromEpochDays(epochDay)
+        val restored = LocalDateConverters.toLocalDate(epochDay)
         TestCase.assertEquals(date, restored)
     }
 
@@ -23,6 +23,6 @@ class LocalDateConvertersTest {
     fun `localDateToEpochDays converts correctly`() {
         val date = LocalDate(2025, 1, 1)
         val epochDay = LocalDateConverters.localDateToEpochDays(date)
-        TestCase.assertEquals(LocalDateConverters.fromEpochDays(epochDay), date)
+        TestCase.assertEquals(LocalDateConverters.toLocalDate(epochDay), date)
     }
 }
